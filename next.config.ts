1125183c32085dab6538a06065c882916c9de707
rzouaoui <rani.zouaoui@capgemini.com>
2025-10-17 10:57:28 +0100

    github actions setup

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     unoptimized: true
-  }
+  },
+  basePath: isProd ? '/website' : '',
+  assetPrefix: isProd ? '/website' : '',
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
